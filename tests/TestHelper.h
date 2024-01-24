7290ef3febcb765a39d71a4c076051839c9c0a75
gen740 <gen740.f@gmail.com>
2024-01-24 16:59:31 +0900

    check tests by clang-tidy

--- tests/TestHelper.h
+++ tests/TestHelper.h
@@ -6,7 +6,6 @@
 template <typename... Args>
 auto createArgcArgv(Args... args) -> std::tuple<int, std::unique_ptr<char*[]>> {
   const size_t N = sizeof...(Args);
-  // char** array = new char*[N];
   auto array = std::make_unique<char*[]>(N);
   size_t i = 0;
   (..., (array[i++] = strdup(args)));
